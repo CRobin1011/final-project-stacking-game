6563c7a2bbc2b83dd22a122757c211f565e52002
Christopher Robin <williamchen1506@gmail.com>
2025-11-17 19:19:27 +0700

    `feat(three)`: initialize Three.js scene with camera, lighting and renderer

---
+++ main.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three'
+import * as CANNON from 'cannon-es'
+
+let camera, scene, renderer
+
+function init() {
+    const aspect = window.innerWidth / window.innerHeight
+    const width = 10
+    const height = width / aspect
+
+    camera = new THREE.OrthographicCamera(
+        width / -2, width / 2, height / 2, height / -2, 0, 100
+    )
+    camera.position.set(4, 4, 4)
+    camera.lookAt(0, 0, 0)
+
+    scene = new THREE.Scene()
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.6)
+    scene.add(ambientLight)
+
+    const dLight = new THREE.DirectionalLight(0xffffff, 0.6)
+    dLight.position.set(10, 20, 0)
+    scene.add(dLight)
+
+    renderer = new THREE.WebGLRenderer({antialias: true})
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.append(renderer.domElement)
+}
+
+init()
